91148908997cb3b48568a0c042cc79d0e0e62d57
司小东 <sixd163@163.com>
2020-03-15 15:22:04 +0900

    123

--- src/app/components/say/say.component.ts
+++ src/app/components/say/say.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit ,Injectable} from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
+
 declare var tinymce: any;
 @Component({
   selector: 'app-say',
@@ -11,8 +12,9 @@ declare var tinymce: any;
 })
 export class SayComponent implements OnInit {
 
-  public editorContent='';
-  public title ='';
+  public editorContent:string='';
+  public title:string ='';
+  public ret:string ='';
   constructor(public http:HttpClient) { 
     
   }
@@ -25,19 +27,24 @@ export class SayComponent implements OnInit {
   postContent(): void {
     console.log(tinymce.activeEditor.getContent({'format':'text'}));
     console.log(this.title);
-    // this.sayInfo = new SayInfo()
-    // this.sayInfo.title = this.title;
-    // this.sayInfo.content = tinymce.activeEditor.getContent({'format':'text'});
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json'})
     };
     var api = "http://127.0.0.1:8088/saveSay"; 
-    this.http.post(api,{"title":this.title,"content":tinymce.activeEditor.getContent({'format':'text'})},httpOptions).subscribe(response => {
-    console.log(response); 
-    });
-    // this.http.post<SayInfo>(api,this.sayInfo,httpOptions).subscribe(response => {
-    //   console.log(response); 
-    // });
-    
+    this.http.post(api,{"title":this.title,"content":tinymce.activeEditor.getContent({'format':'text'})},httpOptions).subscribe(
+      res=>{
+        this.ret = res['ret'];
+      }
+    )
+  this.callback((data)=>{
+     this.ret = data;
+   });
+  }
+
+  callback(cb){
+    setTimeout(function(){ 
+      var retv = '';
+      cb(retv);
+    },1000);
   }
 }
